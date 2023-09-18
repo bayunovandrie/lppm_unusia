c12c0c5dd99212f13ca90d6d547c63476dc470a8
bayu novandrie <bayunovandrie182@gmail.com>
2023-09-18 15:45:20 +0700

    modif file layanan

--- application/views/main/admin/layanan/page_hki_paten.php
+++ application/views/main/admin/layanan/page_hki_paten.php
@@ -26,58 +26,63 @@
                         <?php echo $this->session->flashdata('message'); ?>
                     </div>
                 </div>
-                <button type="button" class="btn btn btn-primary" data-toggle="modal"
-                                    data-target="#modal-new">Add</button>
+
             </div>
             <div class="card-body">
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
                     <thead>
-                            <tr>
-                                <th align="center" style="text-align: center; vertical-align: middle;" width="20">No</th>
-                                <th align="center" style="text-align: center; vertical-align: middle;" >Link Google Form HKI dan Paten</th>
-                                <th align="center" style="text-align: center; vertical-align: middle;" width="100" >Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+                        <tr>
+                            <th align="center" style="text-align: center; vertical-align: middle;" width="20">No</th>
+                            <th align="center" style="text-align: center; vertical-align: middle;">Link Google Form HKI
+                                dan Paten</th>
+                            <th align="center" style="text-align: center; vertical-align: middle;" width="100">Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                             $no = 1;
                             foreach ($hki->result_array() as $n) {
                                 ?>
-                            <tr class="gradeA">
-                                <td ><?php echo $no++; ?></td>
-                                <td ><?= $n['layanan_link'] ?></td>
-                                
-                                <td align="center" style="text-align: center; vertical-align: middle;">
-                                    <button type="button" class="btn btn btn-warning" data-toggle="modal" data-target="#modal-update<?= $n['layanan_id'] ?>"><i class="fas fa-edit"></i></button> | 
-                                    <button type="button" class="btn btn btn-danger" data-toggle="modal" data-target="#modal-delete<?= $n['layanan_id'] ?>"><i class="fas fa-trash"></i></button>
-                                </td>
+                        <tr class="gradeA">
+                            <td><?php echo $no++; ?></td>
+                            <td><?= $n['layanan_link'] ?></td>
 
-                                
-                            </tr>
-                            <?php } ?>
-                        </tbody>
+                            <td align="center" style="text-align: center; vertical-align: middle;">
+                                <button type="button" class="btn btn btn-warning" data-toggle="modal"
+                                    data-target="#modal-update<?= $n['layanan_id'] ?>"><i
+                                        class="fas fa-edit"></i></button>
+                            </td>
+
+
+                        </tr>
+                        <?php } ?>
+                    </tbody>
                 </table>
             </div>
         </div>
-        
+
     </div>
-    
+
 </div>
 
-<div class="modal fade bd-example-modal-lg" id="modal-new" tabindex="-1" role="dialog"
+<?php foreach ($hki->result_array() as $update) { ?>
+<div class="modal fade bd-example-modal-lg" id="modal-update<?= $update['layanan_id'] ?>" tabindex="-1" role="dialog"
     aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Tambah Link Form Layanan HKI dan Paten</h4>
+                <h4 class="modal-title">Edit Link Form Layanan HKI dan Paten</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
-                <form action="<?= BASEURL ?>admin/Layanan/create" method="post" enctype="multipart/form-data">
+                <form action="<?= BASEURL ?>admin/Layanan/update" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
-                      <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
-                      <input type="text" class="form-control" id="exampleFormControlInput1" name="link" required>
-                      <input type="hidden" name="jenis" value="hki">
+                        <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" name="link"
+                            value="<?= $update['layanan_link']; ?>" required>
+                        <input type="hidden" name="id" value="<?= $update['layanan_id']; ?>">
+                        <input type="hidden" name="jenis" value="hki">
                     </div>
             </div>
             <div class="modal-footer">
@@ -89,61 +94,5 @@
         </div>
     </div>
 </div>
-
-<?php foreach ($hki->result_array() as $update) { ?>
-    <div class="modal fade bd-example-modal-lg" id="modal-update<?= $update['layanan_id'] ?>" tabindex="-1" role="dialog"
-    aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Edit Link Form Layanan HKI dan Paten</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?= BASEURL ?>admin/Layanan/update" method="post" enctype="multipart/form-data">
-                        <div class="mb-3">
-                          <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
-                          <input type="text" class="form-control" id="exampleFormControlInput1" name="link" value="<?= $update['layanan_link']; ?>" required>
-                          <input type="hidden" name="id" value="<?= $update['layanan_id']; ?>" >
-                          <input type="hidden" name="jenis" value="hki">
-                        </div>
-                </div>
-                <div class="modal-footer">
-
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-success">Save</button>
-                </div>
-                </form>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php } ?>
-
-<?php foreach ($hki->result_array() as $delete) {?>
-    <div class="modal fade bd-example-modal-lg" id="modal-delete<?= $delete['layanan_id'] ?>" tabindex="-1" role="dialog"
-    aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Delete Link Form Layanan HKI dan Paten</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?= BASEURL ?>admin/Layanan/delete" method="post">
-                        <div class="mb-3">
-                          <p>Apakah Anda Yakin Untuk Delete Link <span style="font-weight: bold;" ><?php echo $delete['layanan_link'] ?></span> </p>
-                          <input type="hidden" name="id" value="<?= $delete['layanan_id']; ?>">
-                          <input type="hidden" name="jenis" value="hki">
-                        </div>
-                </div>
-                <div class="modal-footer">
-
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-success">Save</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
+</div>
 <?php } ?>
--- application/views/main/admin/layanan/page_insentif_jurnal.php
+++ application/views/main/admin/layanan/page_insentif_jurnal.php
@@ -26,58 +26,62 @@
                         <?php echo $this->session->flashdata('message'); ?>
                     </div>
                 </div>
-                <button type="button" class="btn btn btn-primary" data-toggle="modal"
-                                    data-target="#modal-new">Add</button>
             </div>
             <div class="card-body">
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
                     <thead>
-                            <tr>
-                                <th align="center" style="text-align: center; vertical-align: middle;" width="20">No</th>
-                                <th align="center" style="text-align: center; vertical-align: middle;" >Link Google Form Insentif Jurnal</th>
-                                <th align="center" style="text-align: center; vertical-align: middle;" width="100" >Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
+                        <tr>
+                            <th align="center" style="text-align: center; vertical-align: middle;" width="20">No</th>
+                            <th align="center" style="text-align: center; vertical-align: middle;">Link Google Form
+                                Insentif Jurnal</th>
+                            <th align="center" style="text-align: center; vertical-align: middle;" width="100">Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
                             $no = 1;
                             foreach ($insentif->result_array() as $n) {
                                 ?>
-                            <tr class="gradeA">
-                                <td ><?php echo $no++; ?></td>
-                                <td ><?= $n['insentif_link'] ?></td>
-                                
-                                <td align="center" style="text-align: center; vertical-align: middle;">
-                                    <button type="button" class="btn btn btn-warning" data-toggle="modal" data-target="#modal-update<?= $n['insentif_id'] ?>"><i class="fas fa-edit"></i></button> | 
-                                    <button type="button" class="btn btn btn-danger" data-toggle="modal" data-target="#modal-delete<?= $n['insentif_id'] ?>"><i class="fas fa-trash"></i></button>
-                                </td>
+                        <tr class="gradeA">
+                            <td><?php echo $no++; ?></td>
+                            <td><?= $n['insentif_link'] ?></td>
 
-                                
-                            </tr>
-                            <?php } ?>
-                        </tbody>
+                            <td align="center" style="text-align: center; vertical-align: middle;">
+                                <button type="button" class="btn btn btn-warning" data-toggle="modal"
+                                    data-target="#modal-update<?= $n['insentif_id'] ?>"><i
+                                        class="fas fa-edit"></i></button>
+                            </td>
+
+
+                        </tr>
+                        <?php } ?>
+                    </tbody>
                 </table>
             </div>
         </div>
-        
+
     </div>
-    
+
 </div>
 
-<div class="modal fade bd-example-modal-lg" id="modal-new" tabindex="-1" role="dialog"
+<?php foreach ($insentif->result_array() as $update) { ?>
+<div class="modal fade bd-example-modal-lg" id="modal-update<?= $update['insentif_id'] ?>" tabindex="-1" role="dialog"
     aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Tambah Link Form Layanan Insentif Jurnal</h4>
+                <h4 class="modal-title">Edit Link Form Layanan Insentif Jurnal</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
-                <form action="<?= BASEURL ?>admin/Layanan/create" method="post" enctype="multipart/form-data">
+                <form action="<?= BASEURL ?>admin/Layanan/update" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
-                      <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
-                      <input type="text" class="form-control" id="exampleFormControlInput1" name="link" required>
-                      <input type="hidden" name="jenis" value="insentif">
+                        <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" name="link"
+                            value="<?= $update['insentif_link']; ?>" required>
+                        <input type="hidden" name="id" value="<?= $update['insentif_id']; ?>">
+                        <input type="hidden" name="jenis" value="insentif">
                     </div>
             </div>
             <div class="modal-footer">
@@ -89,61 +93,5 @@
         </div>
     </div>
 </div>
-
-<?php foreach ($insentif->result_array() as $update) { ?>
-    <div class="modal fade bd-example-modal-lg" id="modal-update<?= $update['insentif_id'] ?>" tabindex="-1" role="dialog"
-    aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Edit Link Form Layanan Insentif Jurnal</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?= BASEURL ?>admin/Layanan/update" method="post" enctype="multipart/form-data">
-                        <div class="mb-3">
-                          <label for="exampleFormControlInput1" class="form-label">Link Google Form</label>
-                          <input type="text" class="form-control" id="exampleFormControlInput1" name="link" value="<?= $update['insentif_link']; ?>" required>
-                          <input type="hidden" name="id" value="<?= $update['insentif_id']; ?>" >
-                          <input type="hidden" name="jenis" value="insentif">
-                        </div>
-                </div>
-                <div class="modal-footer">
-
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-success">Save</button>
-                </div>
-                </form>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php } ?>
-
-<?php foreach ($insentif->result_array() as $delete) {?>
-    <div class="modal fade bd-example-modal-lg" id="modal-delete<?= $delete['insentif_id'] ?>" tabindex="-1" role="dialog"
-    aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Delete Link Form Layanan Insentif Jurnal</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="<?= BASEURL ?>admin/Layanan/delete" method="post">
-                        <div class="mb-3">
-                          <p>Apakah Anda Yakin Untuk Delete Link <span style="font-weight: bold;" ><?php echo $delete['insentif_link'] ?></span> </p>
-                          <input type="hidden" name="id" value="<?= $delete['insentif_id']; ?>">
-                          <input type="hidden" name="jenis" value="insentif">
-                        </div>
-                </div>
-                <div class="modal-footer">
-
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                    <button type="submit" class="btn btn-success">Save</button>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
+</div>
 <?php } ?>
